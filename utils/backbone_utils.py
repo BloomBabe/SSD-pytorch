c09319326651fc21bc74adffc718e12f7ab9ca6b
BloomBabe <schebnev1998@gmail.com>
2021-02-28 18:57:00 +0000

    Add vgg backbone

---
+++ utils/backbone_utils.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+from torch.hub import load_state_dict_from_url
+import numpy as np 
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def vgg_backbone(name = 'vgg16_bn',
+                 input_channels = 3,
+                 pretrained = True):
+    if (input_channels != 3) and pretrained:
+        raise ValueError('There are available weights only for 3 channels models.')
+    assert name in ['vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
+                    'vgg19', 'vgg19_bn']
+    with open(os.path.join(BASE_DIR,'backbone_cfgs.json')) as f:
+        cfgs = json.load(f)
+    model = VGGBackbone(cfg=cfgs[name], input_channels=input_channels,
+                        pretrained=pretrained)
+    return model
+
+class VGGBackbone(nn.Module):
+    """ Make VGG model without classifier"""
+    def __init__(self,
+                 cfg,
+                 input_channels = 3,
+                 pretrained = True):
+        super(VGGBackbone, self).__init__()
+        self.cfg = cfg 
+        self.input_channels = input_channels
+        self.batch_norm = bool(self.cfg['batch_norm'])
+        self.url = str(self.cfg['url'])
+        self.build = self.cfg['cfg']
+        self.pretrained = pretrained
+
+        self.features = self._make_layers()
+        self._init_weights()
+
+    def _make_layers(self):
+        # https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+        layers: List[nn.Module] = []
+        in_channels = self.input_channels
+        for v in self.build:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                v = int(v)
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if self.batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.ReLU(inplace=True)]
+                in_channels = v
+        return nn.Sequential(*layers)
+
+    def _init_weights(self):
+        if self.pretrained:
+            state_dict = load_state_dict_from_url(self.url,
+                                                  progress=False)
+            self.load_state_dict(state_dict, strict=False)
+        else:    
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
