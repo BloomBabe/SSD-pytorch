3a8116fe47a49e3d670506a8e28de4eae6350f50
BloomBabe <schebnev1998@gmail.com>
2021-03-05 12:16:33 +0000

    Add default boxes

--- utils/modules/box_head.py
+++ utils/modules/box_head.py
@@ -11,11 +11,15 @@ class SSDMultiBox(nn.Module):
         self.num_classes = num_classes
         self.num_boxes = cfg['num_boxes']
         self.input_channels = cfg['in_channels']
+        self.fmap_wh = cfg['fmap_wh']
+        self.aspect_ratios = cfg['aspect_ratios']
+        self.scales = cfg['scales']
         assert len(self.num_boxes) == len(self.input_channels)
 
         self.reg_layers = nn.ModuleList(self._make_locreg())
         self.cls_layers = nn.ModuleList(self._make_cls())
         self._init_weights()
+        self.default_boxes = self._init_default_boxes()
 
     def _make_cls(self):
         """Make classifier head"""
@@ -36,6 +40,34 @@ class SSDMultiBox(nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.xavier_uniform(m.weight.data)
                 m.bias.data.zero_()
+
+    def _init_default_boxes(self):
+        self.aspect_ratios = [[1., a, 1/a] for a in self.aspect_ratios]
+        assert len(self.aspect_ratios)==len(self.fmap_wh)
+        assert len(self.aspect_ratios)==len(self.scales)
+        default_boxes = []
+        for k in range(len(self.fmap_wh)):
+            for i in range(self.fmap_wh[k]):
+                for j in range(self.fmap_wh[k]):
+                    cx = (j + 0.5) / self.fmap_wh[k]
+                    cy = (i + 0.5) / self.fmap_wh[k]
+
+                    for ratio in self.aspect_ratios[k]:
+                        #(cx, cy, w, h)
+                        default_boxes.append([cx, cy, self.scales[k]* math.sqrt(k), 
+                                              self.scales[k]/math.sqrt(ratio)]) 
+                        
+                        if ratio == 1:
+                            try:
+                                add_scale = math.sqrt(self.scales[k]*self.scales[fmaps[k+1]])
+                            except IndexError:
+                                #for the last feature map
+                                add_scale = 1.
+                            default_boxes.append([cx, cy, add_scale, add_scale])
+        default_boxes = torch.FloatTensor(default_boxes).to(device) #(8732, 4)
+        default_boxes.clamp_(0, 1)
+        return default_boxes
+
 
     def forward(self, list_x):
         assert (len(list_x) == len(self.reg_layers)) and(len(list_x) == len(self.cls_layers))
