611004db72c5e0b0624e2404b174b0566696a436
BloomBabe <schebnev1998@gmail.com>
2021-03-07 15:09:58 +0000

    Add train

--- train.py
+++ train.py
@@ -14,3 +14,17 @@ ap.add_argument("--iterations", default= 145000, type= int, help = "number of it
 ap.add_argument("--grad_clip", default = None, help= "Gradient clip for large batch_size")
 ap.add_argument("--adjust_optim", default = None, help = "Adjust optimizer for checkpoint model")
 args = ap.parse_args()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+checkpoint = args.checkpoint
+batch_size = args.batch_size  # batch size
+iterations = args.iterations  # number of iterations to train
+workers = args.num_workers  # number of workers for loading data in the DataLoader
+print_freq = 100  # print training status every __ batches
+lr = args.lr  # learning rate
+decay_lr_at = [96500, 120000]  # decay learning rate after these many iterations
+decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate
+momentum = args.momentum  # momentum
+weight_decay = args.weight_decay
+grad_clip = args.grad_clip
