603a2e5c6472de5b16bd3fb0fc09ec8844165612
BloomBabe <schebnev1998@gmail.com>
2021-03-07 22:05:19 +0000

    Add augmentation

--- utils/datasets/augmentation.py
+++ utils/datasets/augmentation.py
@@ -5,6 +5,7 @@ import types
 import random
 import torchvision.transforms.functional as TF
 import PIL
+from utils.box_utils import compute_iou
 
 random.seed(42)
 
@@ -151,6 +152,7 @@ class RandomCrop(object):
     def __call__(self, image, boxes, labels):
         width = image.width
         height = image.height
+        image = TF.to_tensor(image)
         while True:
             mode = random.choice(self.sample_options)
 
@@ -164,8 +166,6 @@ class RandomCrop(object):
                 max_iou = float('inf')
             # max trails (50)
             for _ in range(50):
-                current_image = image
-
                 w = random.uniform(0.3 * width, width)
                 h = random.uniform(0.3 * height, height)
 
@@ -177,53 +177,46 @@ class RandomCrop(object):
                 top = random.uniform(height - h)
 
                 # convert to integer rect x1,y1,x2,y2
-                rect = np.array([int(left), int(top), int(left+w), int(top+h)])
-
+                crop = torch.FloatTensor([int(left), int(top), int(left+w), int(top+h)])
                 # calculate IoU (jaccard overlap) b/t the cropped and gt boxes
-                overlap = jaccard_numpy(boxes, rect)
-
+                overlap = compute_iou(boxes, crop)
+                overlap = overlap.squeeze(0)
                 # is min and max overlap constraint satisfied? if not try again
-                if overlap.min() < min_iou and max_iou < overlap.max():
+                if overlap.min().item() < min_iou and max_iou < overlap.max().item():
                     continue
-
                 # cut the crop from the image
-                current_image = current_image[rect[1]:rect[3], rect[0]:rect[2],
-                                              :]
-
+                new_image = image[:, int(top):int(top+h), int(left):int(left+w)]
                 # keep overlap with gt box IF center in sampled patch
                 centers = (boxes[:, :2] + boxes[:, 2:]) / 2.0
 
                 # mask in all gt boxes that above and to the left of centers
-                m1 = (rect[0] < centers[:, 0]) * (rect[1] < centers[:, 1])
-
+                m1 = (crop[0] < centers[:, 0]) * (crop[1] < centers[:, 1])
                 # mask in all gt boxes that under and to the right of centers
-                m2 = (rect[2] > centers[:, 0]) * (rect[3] > centers[:, 1])
+                m2 = (crop[2] > centers[:, 0]) * (crop[3] > centers[:, 1])
 
                 # mask in that both m1 and m2 are true
                 mask = m1 * m2
-
                 # have any valid boxes? try again if not
                 if not mask.any():
                     continue
 
                 # take only matching gt boxes
                 current_boxes = boxes[mask, :].copy()
-
                 # take only matching gt labels
                 current_labels = labels[mask]
 
                 # should we use the box left and top corner or the crop's
-                current_boxes[:, :2] = np.maximum(current_boxes[:, :2],
-                                                  rect[:2])
+                current_boxes[:, :2] = torch.max(current_boxes[:, :2],
+                                                  crop[:2])
                 # adjust to crop (by substracting crop's left,top)
-                current_boxes[:, :2] -= rect[:2]
+                current_boxes[:, :2] -= crop[:2]
 
-                current_boxes[:, 2:] = np.minimum(current_boxes[:, 2:],
-                                                  rect[2:])
+                current_boxes[:, 2:] = torch.min(current_boxes[:, 2:],
+                                                  crop[2:])
                 # adjust to crop (by substracting crop's left,top)
-                current_boxes[:, 2:] -= rect[:2]
+                current_boxes[:, 2:] -= crop[:2]
 
-                return current_image, current_boxes, current_labels
+                return new_image, current_boxes, current_labels
 
 class SSDAugmentation(object):
     def __init__(self, size=300, mean = [0.485, 0.456, 0.406],
